d4919b3bf865afe6138aa1242670ea94f2245b9e
Bascomb Arnette <bascomb.arnette.pro@icloud.com>
2020-01-17 09:46:18 -0800

    gitignore

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,41 @@
 package org.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.client.ApiClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.Configuration;
+import io.swagger.client.api.PetApi;
+import io.swagger.client.auth.OAuth;
+import io.swagger.client.model.Pet;
 
 /**
  * Hello world!
  */
-@SpringBootApplication
+//@SpringBootApplication
+//public class App {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        SpringApplication.run(App.class, args);
+//
+//
+//    }
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        SpringApplication.run(App.class, args);
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
 
+        // Configure OAuth2 access token for authorization: petstore_auth
+        OAuth petstore_auth = (OAuth) defaultClient.getAuthentication("petstore_auth");
+        petstore_auth.setAccessToken("YOUR ACCESS TOKEN");
 
+        PetApi apiInstance = new PetApi();
+        Pet body = new Pet(); // Pet | Pet object that needs to be added to the store
+        try {
+            apiInstance.addPet(body);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling PetApi#addPet");
+            e.printStackTrace();
+        }
     }
-
-
 }
+
